0dcc36729f57a3c530e651e41edaa29d0b995b82
Docjar99 <jaardeltacoch@gmail.com>
2025-04-06 16:55:00 -0500

    iconos funcionando

--- CatoBox/components/Icons.jsx
+++ CatoBox/components/Icons.jsx
@@ -1,9 +1,9 @@
 import Octicons from '@expo/vector-icons/Octicons';
 
 export const HomeIcon=(props)=>{
-<Octicons name="home" size={24} color="black" />
+return <Octicons name="home" size={24} color="black" />
 };
 
 export const CircleInfoIcon=(props)=>{
-<Octicons name="info" size={24} color="black" />
+return <Octicons name="info" size={24} color="black" />
 };
